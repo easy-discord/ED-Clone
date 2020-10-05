40028772e3272f283f15cb722db668360306f0c0
Seer <50992478+dxy-Seer@users.noreply.github.com>
2020-10-05 20:29:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const discordBot = require("./bot");
 app.use(require('./guides'));
 app.use(express.static("public"));
-setInterval(() => require('node-fetch')(config.site.Url).then(() => console.log('Pinged website....')), 4 * 60 * 1000);
+setInterval(() => require('node-fetch')(config.site.url).then(() => console.log('Pinged website....')), 4 * 60 * 1000);
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
