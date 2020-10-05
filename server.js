efd3edec3be4bb8a431a7c499fe1a4d455aafe15
Seer <50992478+dxy-Seer@users.noreply.github.com>
2020-10-05 09:14:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const fs = require("fs");
-const discordBotkit = require("botkit-discord");
-var Client = require("uptime-robot");
+const config = require('@dxy_seer/easydiscord/package.json');
 const app = express();
 const discordBot = require("./bot");
 app.use(require('./guides'));
 app.use(express.static("public"));
-setInterval(() => require('node-fetch')('https://easydiscord.glitch.me/').then(() => console.log('Pinged website....')), 4 * 60 * 1000);
+setInterval(() => require('node-fetch')(config.site.Url).then(() => console.log('Pinged website....')), 4 * 60 * 1000);
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
